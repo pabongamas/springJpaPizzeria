406ae49f70ab333916ae41d70d6f64dbde7bd563
root <root@jairo.pabon>
2024-03-08 18:09:46 -0500

    security}

---
+++ src/main/java/com/platzi/platzipizzeria/web/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.platzi.platzipizzeria.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    //establece la configuracion inicial por defectro de  la aplicacion ,en esta estamos haciendo un filtro de segurdad en el que interceptamos
+    // las http request y autorizamos cualquiere peticion el cual ese autenticado , esta autenticacion aca es la Basic 
+    // donde se le envia el usuario,este dato lo genera spring una vez se ejecute la aplicacion en desarrollo
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(customizeRequests -> {
+                customizeRequests
+                        .anyRequest()
+                        .authenticated();
+                }
+            )
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults());
+        
+        return http.build();
+    }
+}
