f3743920362c823909e57a8f77445930acb8c512
root <root@jairo.pabon>
2024-03-12 17:56:48 -0500

    actualizacion

--- src/main/java/com/platzi/platzipizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/platzipizzeria/web/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,8 +26,10 @@ public class SecurityConfig {
                 customizeRequests
                         // .anyRequest()
                         // .authenticated()
-                        .requestMatchers(HttpMethod.GET,"/pizzeria/api/pizzas/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT).denyAll()
+                        .requestMatchers(HttpMethod.GET,"/pizzas/**").hasAnyRole("ADMIN","CUSTOMER")
+                        .requestMatchers(HttpMethod.POST,"/pizzas/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                        .requestMatchers("/orders/**").hasRole("ADMIN")
                         .anyRequest().authenticated();
                 }
             )
@@ -30,5 +38,32 @@ public class SecurityConfig {
             .httpBasic(Customizer.withDefaults());
         
         return http.build();
+    }
+
+
+    //esto se hace para gestionar usuariores en memoria , le pasamos el nombre de user , la password con el tipo de 
+    // encode para encryptar, para el rol ADMIN ,SE PRUEBA CON LA RUTA  http://localhost:8090/pizzeria/api/orders SI NO SE LE PASA 
+    // BASIC AUTH CON LA INFO DEL USUARIO EN MEMORIA  VA A SALIR NO AUTORIZADO 
+    @Bean
+    public UserDetailsService memoryUsers(){
+        UserDetails admin= User.builder()
+            .username("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles("ADMIN")
+            .build();
+
+
+            UserDetails customer= User.builder()
+            .username("customer")
+            .password(passwordEncoder().encode("customer123"))
+            .roles("CUSTOMER")
+            .build();
+
+            return new InMemoryUserDetailsManager(admin,customer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
